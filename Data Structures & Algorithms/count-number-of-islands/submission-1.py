2ece71d96ccf70f0afe1d4e2c812a916f1f53476
David M. Jentjens <davidjentjens@gmail.com>
2026-06-09 00:04:55 -0300

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        grid_height, grid_width = len(grid), len(grid[0])
+
+        def getValidNeighbors(start_pos):
+            i, j = start_pos
+            valid_neighbors = []
+            for pos in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                if 0 <= pos[0] < grid_height and 0 <= pos[1] < grid_width and grid[pos[0]][pos[1]] == '1':
+                    valid_neighbors.append(pos)
+            return valid_neighbors
+
+        def dfs(start_index):
+            i, j = start_index
+            if grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            for neighbor in getValidNeighbors((i, j)):
+                dfs(neighbor)
+
+        num_islands = 0
+
+        for i in range(grid_height):
+            for j in range(grid_width):
+                if grid[i][j] == "1":
+                    dfs((i,j))
+                    num_islands += 1
+
+        return num_islands
