65bc2f03e17f532932b130aeb744ee883732668b
David M. Jentjens <davidjentjens@gmail.com>
2026-06-08 21:16:41 -0300

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()     
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(start_index, node):
+            if start_index == len(word):
+                return node.end_of_word
+
+            char = word[start_index]
+
+            if char == '.':
+                for child_char in node.children:
+                    if dfs(start_index + 1, node.children[child_char]):
+                        return True
+                return False
+
+            if char not in node.children:
+                return False
+            return dfs(start_index + 1, node.children[char])
+
+        return dfs(0, self.root)
