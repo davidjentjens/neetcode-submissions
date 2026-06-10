703614e387c7c53aedd619d4a035d2a0135c91b9
David M. Jentjens <davidjentjens@gmail.com>
2026-06-09 22:37:47 -0300

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node_original: Optional['Node']) -> Optional['Node']:
+        visited = {}
+
+        if not node_original:
+            return None
+
+        def dfs(node: Node) -> Optional['Node']:
+            # Node is already visited, we must return it's respective clone
+            if node.val in visited:
+                return visited[node.val]
+
+            # We then need to mark the current node as visited
+            copy_node = Node(node.val)
+            visited[node.val] = copy_node
+
+            for neighbor in node.neighbors:
+                copy_node.neighbors.append(dfs(neighbor))
+            
+            return copy_node
+
+        return dfs(node_original)
